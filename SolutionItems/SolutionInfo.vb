107e78f9dfb823e4e0059dd69b4330b2a84df03d
maske <s@maske.one>
2019-08-02 20:13:17 +0200

    sln additions

---
+++ SolutionItems/SolutionInfo.vb
@@ -0,0 +1,23 @@
+﻿Imports System.Reflection
+Imports System.Security
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
+<Assembly: AssemblyConfiguration("DEV")> 
+
+
+'' System.Security
+''<Assembly: AllowPartiallyTrustedCallers> 
+<Assembly: SecurityRules(SecurityRuleSet.Level1)> 
+
+
+Friend Module SolutionDescription
+
+    Public Const Copyright As String = "maske.one"
+
+    Public Const Trademark As String = "NETBase"
+
+    Public Const Company As String = "maske.one"
+
+    Public Const Product As String = "NETBase Framework"
+End Module
