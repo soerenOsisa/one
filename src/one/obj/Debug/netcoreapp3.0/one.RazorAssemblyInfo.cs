0c0a14c06781b289979d7ad76ee59ebf73b1a6da
maske <s@maske.one>
2019-08-02 20:40:23 +0200

    even more deleted

--- src/one/obj/Debug/netcoreapp3.0/one.RazorAssemblyInfo.cs
+++ src/one/obj/Debug/netcoreapp3.0/one.RazorAssemblyInfo.cs
@@ -11,13 +11,7 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("one")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
-[assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("one")]
-[assembly: System.Reflection.AssemblyTitleAttribute("one")]
-[assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
+[assembly: Microsoft.AspNetCore.Mvc.ApplicationParts.RelatedAssemblyAttribute("one.Views")]
 
 // Von der MSBuild WriteCodeFragment-Klasse generiert.
 
